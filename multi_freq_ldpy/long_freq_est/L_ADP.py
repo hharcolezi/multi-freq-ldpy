4a3e124a69762070c3d134e34b69221534270f2f
Héber Hwang Arcolezi <55852688+hharcolezi@users.noreply.github.com>
2022-04-14 13:29:28 +0200

    Update L_ADP.py

--- multi_freq_ldpy/long_freq_est/L_ADP.py
+++ multi_freq_ldpy/long_freq_est/L_ADP.py
@@ -3,7 +3,19 @@ from long_freq_est.L_GRR import L_GRR_Client, L_GRR_Aggregator
 from long_freq_est.L_OSUE import L_OSUE_Client, L_OSUE_Aggregator
 from long_freq_est.Variance_LONG_PURE import VAR_Long_Pure
 
+# [1] Arcolezi et al (2021) "Improving the Utility of Locally Differentially Private Protocols for Longitudinal and Multidimensional Frequency Estimates" (arXiv:2111.04636).
+
 def L_ADP_Client(input_data, k, eps_perm, eps_1):
+
+    """
+    Longitudinal Adaptive (L-ADP) protocol that minimizes variance value (i.e., either L-GRR or L-OSUE from [1], a.k.a. ALLOMFREE).
+
+    :param input_data: user's true value;
+    :param k: attribute's domain size;
+    :param eps_perm: upper bound of privacy guarantee (infinity reports);
+    :param eps_1: lower bound of privacy guarantee (a single report), thus, eps_1 < eps_perm;
+    :return: sanitized value or UE vector.
+    """
 
     # GRR parameters for round 1
     p1_grr = np.exp(eps_perm) / (np.exp(eps_perm) + k - 1)
@@ -19,11 +31,11 @@ def L_ADP_Client(input_data, k, eps_perm, eps_1):
     else:
         raise ValueError('Probabilities are negative.')
 
-    # OUE parameters
+    # OUE parameters for round 1
     p1_ue = 1 / 2
     q1_ue = 1 / (np.exp(eps_perm) + 1)
 
-    # SUE parameters
+    # SUE parameters for round 2
     p2_ue = (1 - np.exp(eps_1 + eps_perm)) / (np.exp(eps_1) - np.exp(eps_perm) - np.exp(eps_1 + eps_perm) + 1)
     q2_ue = 1 - p2_ue
 
@@ -32,9 +44,11 @@ def L_ADP_Client(input_data, k, eps_perm, eps_1):
     else:
         raise ValueError('Probabilities are negative.')
 
+    # Variance values of L-GRR and L-OSUE
     var_l_grr = VAR_Long_Pure(p1_grr, q1_grr, p2_grr, q2_grr)
     var_l_osue = VAR_Long_Pure(p1_ue, q1_ue, p2_ue, q2_ue)
 
+    # Adaptive longitudinal protocol (a.k.a. ALLOMFREE in [1])
     if var_l_grr <= var_l_osue:
 
         return L_GRR_Client(input_data, k, eps_perm, eps_1)
@@ -44,7 +58,15 @@ def L_ADP_Client(input_data, k, eps_perm, eps_1):
 
 def L_ADP_Aggregator(reports, k, eps_perm, eps_1):
 
-    n = len(reports)
+    """
+    Statistical Estimator for Normalized Frequency (0 -- 1) with post-processing to ensure non-negativity.
+
+    :param reports: list of all sanitized L-GRR values or L-OSUE UE-based vectors;
+    :param k: attribute's domain size;
+    :param eps_perm: upper bound of privacy guarantee (infinity reports);
+    :param eps_1: lower bound of privacy guarantee (a single report), thus, eps_1 < eps_perm;
+    :return: normalized frequency (histogram) estimation.
+    """
 
     # GRR parameters for round 1
     p1_grr = np.exp(eps_perm) / (np.exp(eps_perm) + k - 1)
@@ -60,11 +82,11 @@ def L_ADP_Aggregator(reports, k, eps_perm, eps_1):
     else:
         raise ValueError('Probabilities are negative.')
 
-    # OUE parameters
+    # OUE parameters for round 1
     p1_ue = 1 / 2
     q1_ue = 1 / (np.exp(eps_perm) + 1)
 
-    # SUE parameters
+    # SUE parameters for round 2
     p2_ue = (1 - np.exp(eps_1 + eps_perm)) / (np.exp(eps_1) - np.exp(eps_perm) - np.exp(eps_1 + eps_perm) + 1)
     q2_ue = 1 - p2_ue
 
@@ -73,25 +95,14 @@ def L_ADP_Aggregator(reports, k, eps_perm, eps_1):
     else:
         raise ValueError('Probabilities are negative.')
 
+    # Variance values of L-GRR and L-OSUE
     var_l_grr = VAR_Long_Pure(p1_grr, q1_grr, p2_grr, q2_grr)
     var_l_osue = VAR_Long_Pure(p1_ue, q1_ue, p2_ue, q2_ue)
 
+    # Adaptive estimator
     if var_l_grr <= var_l_osue:
-        count_report = np.zeros(k)
-        for rep in reports:  # how many times each value has been reported
-            count_report[rep] += 1
 
-        est_freq = ((count_report - n * q1_grr * (p2_grr - q2_grr) - n * q2_grr) / (n * (p1_grr - q1_grr) * (p2_grr - q2_grr))).clip(0)
+        return L_GRR_Aggregator(reports, k, eps_perm, eps_1)
 
     else:
-        est_freq = ((sum(reports) - n * q1_ue * (p2_ue - q2_ue) - n * q2_ue) / (n * (p1_ue - q1_ue) * (p2_ue - q2_ue))).clip(0)
-
-    norm_est_freq = est_freq / sum(est_freq)  # re-normalized estimated frequency
-
-    return norm_est_freq
-
-
-
-
-
-
+        return L_OSUE_Aggregator(reports, eps_perm, eps_1)
