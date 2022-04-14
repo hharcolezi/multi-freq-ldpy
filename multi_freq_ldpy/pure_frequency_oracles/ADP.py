7a3891e1de6d239bb365fae6bad30db530d1c1c8
Héber Hwang Arcolezi <55852688+hharcolezi@users.noreply.github.com>
2022-04-14 13:36:23 +0200

    Update ADP.py

--- multi_freq_ldpy/pure_frequency_oracles/ADP.py
+++ multi_freq_ldpy/pure_frequency_oracles/ADP.py
@@ -3,47 +3,95 @@ from pure_frequency_oracles.GRR import GRR_Client, GRR_Aggregator
 from pure_frequency_oracles.UE import UE_Client, UE_Aggregator
 from pure_frequency_oracles.Variance_PURE import VAR_Pure
 
+# [1] Wang et al (2017) "Locally differentially private protocols for frequency estimation" (USENIX Security).
+
 def ADP_Client(input_data, k, epsilon, optimal=True):
 
-    p_grr = np.exp(epsilon) / (np.exp(epsilon) + k - 1)
-    q_grr = (1 - p_grr) / (k - 1)
-    var_grr = VAR_Pure(p_grr, q_grr)
+    """
+    Adaptive (ADP) protocol that minimizes variance value (i.e., either GRR or OUE from [1]).
 
-    p_ue = np.exp(epsilon / 2) / (np.exp(epsilon / 2) + 1)
-    q_ue = 1 - p_ue
+    :param input_data: user's true value;
+    :param k: attribute's domain size;
+    :param epsilon: privacy guarantee;
+    :param optimal: if True, it uses the Optimized UE (OUE) protocol from [2];
+    :return: sanitized value or UE vector.
+    """
 
-    if optimal:
-        p_ue = 1 / 2
-        q_ue = 1 / (np.exp(epsilon) + 1)
+    if epsilon is not None or k is not None:
 
-    var_ue = VAR_Pure(p_ue, q_ue)
+        # GRR parameters
+        p_grr = np.exp(epsilon) / (np.exp(epsilon) + k - 1)
+        q_grr = (1 - p_grr) / (k - 1)
 
-    if var_grr <= var_ue:
+        # Symmetric parameters (p+q = 1)
+        p_ue = np.exp(epsilon / 2) / (np.exp(epsilon / 2) + 1)
+        q_ue = 1 - p_ue
 
-        return GRR_Client(input_data, k, epsilon)
+        # Optimized parameters
+        if optimal:
+            p_ue = 1 / 2
+            q_ue = 1 / (np.exp(epsilon) + 1)
+
+        # Variance values
+        var_grr = VAR_Pure(p_grr, q_grr)
+        var_ue = VAR_Pure(p_ue, q_ue)
+
+        # Adaptive protocol
+        if var_grr <= var_ue:
+
+            return GRR_Client(input_data, k, epsilon)
+        else:
+
+            return UE_Client(input_data, k, epsilon, optimal)
+
     else:
-
-        return UE_Client(input_data, k, epsilon, optimal)
+        raise ValueError('k (int) and epsilon (float) need a numerical value.')
 
 
 def ADP_Aggregator(reports, k, epsilon, optimal=True):
 
-    p_grr = np.exp(epsilon) / (np.exp(epsilon) + k - 1)
-    q_grr = (1 - p_grr) / (k - 1)
-    var_grr = VAR_Pure(p_grr, q_grr)
+    """
+    Statistical Estimator for Normalized Frequency (0 -- 1) with post-processing to ensure non-negativity.
 
-    p_ue = np.exp(epsilon / 2) / (np.exp(epsilon / 2) + 1)
-    q_ue = 1 - p_ue
+    :param reports: list of all GRR sanitized values or UE-based vectors;
+    :param k: attribute's domain size;
+    :param epsilon: privacy guarantee;
+    :param optimal: if True, it uses the Optimized UE (OUE) protocol from [2];
+    :return: normalized frequency (histogram) estimation.
+    """
 
-    if optimal:
-        p_ue = 1 / 2
-        q_ue = 1 / (np.exp(epsilon) + 1)
+    if len(reports) == 0:
 
-    var_ue = VAR_Pure(p_ue, q_ue)
+        raise ValueError('List of reports is empty.')
 
-    if var_grr <= var_ue:
-
-        return GRR_Aggregator(reports, k, epsilon)
     else:
 
-        return UE_Aggregator(reports, epsilon, optimal)
+        if epsilon is not None or k is not None:
+
+            # GRR parameters
+            p_grr = np.exp(epsilon) / (np.exp(epsilon) + k - 1)
+            q_grr = (1 - p_grr) / (k - 1)
+
+            # Symmetric parameters (p+q = 1)
+            p_ue = np.exp(epsilon / 2) / (np.exp(epsilon / 2) + 1)
+            q_ue = 1 - p_ue
+
+            # Optimized parameters
+            if optimal:
+                p_ue = 1 / 2
+                q_ue = 1 / (np.exp(epsilon) + 1)
+
+            # Variance values
+            var_grr = VAR_Pure(p_grr, q_grr)
+            var_ue = VAR_Pure(p_ue, q_ue)
+
+            # Adaptive estimator
+            if var_grr <= var_ue:
+
+                return GRR_Aggregator(reports, k, epsilon)
+            else:
+
+                return UE_Aggregator(reports, epsilon, optimal)
+
+        else:
+            raise ValueError('k (int) and epsilon (float) need a numerical value.')
