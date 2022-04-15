68989b4e4502aca2104887c30f29eef03578d4f5
Héber Hwang Arcolezi <55852688+hharcolezi@users.noreply.github.com>
2022-04-15 17:22:13 +0200

    Add files via upload

---
+++ src/multi_freq_ldpy/long_freq_est/L_OUE.py
@@ -0,0 +1,106 @@
+import numpy as np
+
+# [1] Arcolezi et al (2021) "Improving the Utility of Locally Differentially Private Protocols for Longitudinal and Multidimensional Frequency Estimates" (arXiv:2111.04636).
+# [2] Wang et al (2017) "Locally differentially private protocols for frequency estimation" (USENIX Security).
+
+def L_OUE_Client(input_data, k, eps_perm, eps_1):
+
+    """
+    Longitudinal OUE (L-OUE) [1] protocol that chaines OUE [2] for both first and second rounds of sanitization.
+
+    :param input_data: user's true value;
+    :param k: attribute's domain size;
+    :param eps_perm: upper bound of privacy guarantee (infinity reports);
+    :param eps_1: lower bound of privacy guarantee (a single report), thus, eps_1 < eps_perm;
+    :return: sanitized value or UE vector.
+    """
+
+    # OUE parameters for round 1
+    p1 = 0.5
+    q1 = 1 / (np.exp(eps_perm) + 1)
+
+    # OUE parameters for round 2
+    p2 = 0.5
+    q2 = ((-1.11803398874989 * np.sqrt(
+        0.1 * np.exp(eps_1) + 0.05 * np.exp(2 * eps_1) + 0.3 * np.exp(eps_perm) + 0.45 * np.exp(2 * eps_perm) - np.exp(
+            eps_1 + eps_perm) - 0.7 * np.exp(eps_1 + 2 * eps_perm) + 0.3 * np.exp(2 * eps_1 + eps_perm) + 0.45 * np.exp(
+            2.0 * eps_1 + 2.0 * eps_perm) + 0.05) - 0.25 * np.exp(eps_1) - 0.25 * np.exp(eps_perm) + 0.75 * np.exp(
+        eps_1 + eps_perm) - 0.25) * np.exp(-eps_perm)) / (np.exp(eps_1) - 1.0)
+
+    if (np.array([p1, q1, p2, q2]) >= 0).all():
+        pass
+    else:
+        raise ValueError('Probabilities are negative.')
+
+    # Unary encoding
+    input_ue_data = np.zeros(k)
+    if input_data != None:
+        input_ue_data[input_data] = 1
+
+    # First round of sanitization (permanent memoization) with OUE using user's input_ue_data
+    first_sanitization = np.zeros(k)
+    for ind in range(k):
+        if input_ue_data[ind] != 1:
+            rnd = np.random.random()
+            if rnd <= q1:
+                first_sanitization[ind] = 1
+        else:
+            rnd = np.random.random()
+            if rnd <= p1:
+                first_sanitization[ind] = 1
+
+    # Second round of sanitization with OUE using first_sanitization as input
+    second_sanitization = np.zeros(k)
+    for ind in range(k):
+        if first_sanitization[ind] != 1:
+            rnd = np.random.random()
+            if rnd <= q2:
+                second_sanitization[ind] = 1
+        else:
+            rnd = np.random.random()
+            if rnd <= p2:
+                second_sanitization[ind] = 1
+
+    return second_sanitization
+
+def L_OUE_Aggregator(ue_reports, eps_perm, eps_1):
+
+    """
+    Statistical Estimator for Normalized Frequency (0 -- 1) with post-processing to ensure non-negativity.
+
+    :param reports: list of all L-OUE sanitized UE-based vectors;
+    :param eps_perm: upper bound of privacy guarantee (infinity reports);
+    :param eps_1: lower bound of privacy guarantee (a single report), thus, eps_1 < eps_perm;
+    :return: normalized frequency (histogram) estimation.
+    """
+
+    if len(ue_reports) == 0:
+        raise ValueError('List of reports is empty.')
+
+    # Number of reports
+    n = len(ue_reports)
+
+    # OUE parameters for round 1
+    p1 = 0.5
+    q1 = 1 / (np.exp(eps_perm) + 1)
+
+    # OUE parameters for round 2
+    p2 = 0.5
+    q2 = ((-1.11803398874989 * np.sqrt(
+        0.1 * np.exp(eps_1) + 0.05 * np.exp(2 * eps_1) + 0.3 * np.exp(eps_perm) + 0.45 * np.exp(2 * eps_perm) - np.exp(
+            eps_1 + eps_perm) - 0.7 * np.exp(eps_1 + 2 * eps_perm) + 0.3 * np.exp(2 * eps_1 + eps_perm) + 0.45 * np.exp(
+            2.0 * eps_1 + 2.0 * eps_perm) + 0.05) - 0.25 * np.exp(eps_1) - 0.25 * np.exp(eps_perm) + 0.75 * np.exp(
+        eps_1 + eps_perm) - 0.25) * np.exp(-eps_perm)) / (np.exp(eps_1) - 1.0)
+
+    if (np.array([p1, q1, p2, q2]) >= 0).all():
+        pass
+    else:
+        raise ValueError('Probabilities are negative.')
+
+    # Ensure non-negativity of estimated frequency
+    est_freq = ((sum(ue_reports) - n * q1 * (p2 - q2) - n * q2) / (n * (p1 - q1) * (p2 - q2))).clip(0)
+
+    # Re-normalized estimated frequency
+    norm_est_freq = est_freq / sum(est_freq)
+
+    return norm_est_freq
---
+++ src/multi_freq_ldpy/long_freq_est/L_SOUE.py
@@ -0,0 +1,145 @@
+import numpy as np
+
+# [1] Arcolezi et al (2021) "Improving the Utility of Locally Differentially Private Protocols for Longitudinal and Multidimensional Frequency Estimates" (arXiv:2111.04636).
+# [2] Wang et al (2017) "Locally differentially private protocols for frequency estimation" (USENIX Security).
+
+def L_SOUE_Client(input_data, k, eps_perm, eps_1):
+
+    """
+    Longitudinal SUE-OUE (L-SOUE) [1] protocol that chaines SUE [2] for first round and OUE [2] for second round of sanitization.
+
+    :param input_data: user's true value;
+    :param k: attribute's domain size;
+    :param eps_perm: upper bound of privacy guarantee (infinity reports);
+    :param eps_1: lower bound of privacy guarantee (a single report), thus, eps_1 < eps_perm;
+    :return: sanitized value or UE vector.
+    """
+
+    # SUE parameters for round 1
+    p1 = np.exp(eps_perm / 2) / (np.exp(eps_perm / 2) + 1)
+    q1 = 1 - p1
+
+    # OUE parameters for round 2
+    p2 = 0.5
+    q2 = (3.35410196624968 * (np.exp(eps_1) - 1) * (
+                np.exp(0.5 * eps_perm) + 2 * np.exp(eps_perm) + np.exp(1.5 * eps_perm)) * np.sqrt(
+        0.0111111111111111 * np.exp(eps_1) + 0.00555555555555556 * np.exp(2 * eps_1) + 0.0444444444444444 * np.exp(
+            0.5 * eps_perm) + 0.155555555555556 * np.exp(eps_perm) + 0.311111111111111 * np.exp(
+            1.5 * eps_perm) + 0.388888888888889 * np.exp(2 * eps_perm) + 0.311111111111111 * np.exp(
+            2.5 * eps_perm) + 0.155555555555556 * np.exp(3 * eps_perm) + 0.0444444444444444 * np.exp(
+            3.5 * eps_perm) + 0.00555555555555556 * np.exp(4 * eps_perm) - 0.222222222222222 * np.exp(
+            eps_1 + eps_perm) - 0.711111111111111 * np.exp(eps_1 + 1.5 * eps_perm) - np.exp(
+            eps_1 + 2 * eps_perm) - 0.711111111111111 * np.exp(eps_1 + 2.5 * eps_perm) - 0.222222222222222 * np.exp(
+            eps_1 + 3 * eps_perm) + 0.0111111111111111 * np.exp(eps_1 + 4 * eps_perm) + 0.0444444444444444 * np.exp(
+            2 * eps_1 + 0.5 * eps_perm) + 0.155555555555556 * np.exp(2 * eps_1 + eps_perm) + 0.311111111111111 * np.exp(
+            2 * eps_1 + 1.5 * eps_perm) + 0.388888888888889 * np.exp(
+            2 * eps_1 + 2 * eps_perm) + 0.311111111111111 * np.exp(
+            2 * eps_1 + 2.5 * eps_perm) + 0.155555555555556 * np.exp(
+            2 * eps_1 + 3 * eps_perm) + 0.0444444444444444 * np.exp(
+            2 * eps_1 + 3.5 * eps_perm) + 0.00555555555555556 * np.exp(
+            2 * eps_1 + 4 * eps_perm) + 0.00555555555555556) - 0.25 * (
+                      np.exp(0.5 * eps_perm) + 2 * np.exp(eps_perm) + np.exp(1.5 * eps_perm) - np.exp(
+                  eps_1 + 0.5 * eps_perm) - 2 * np.exp(eps_1 + eps_perm) - np.exp(eps_1 + 1.5 * eps_perm)) * (
+                      np.exp(eps_1) + 4 * np.exp(0.5 * eps_perm) + 4 * np.exp(eps_perm) - np.exp(
+                  2 * eps_perm) - 4 * np.exp(eps_1 + eps_perm) - 4 * np.exp(eps_1 + 1.5 * eps_perm) - np.exp(
+                  eps_1 + 2 * eps_perm) + 1)) / (
+                     (np.exp(eps_1) - 1) * (np.exp(0.5 * eps_perm) + 2 * np.exp(eps_perm) + np.exp(1.5 * eps_perm)) * (
+                         np.exp(0.5 * eps_perm) + 2 * np.exp(eps_perm) + np.exp(1.5 * eps_perm) - np.exp(
+                     eps_1 + 0.5 * eps_perm) - 2 * np.exp(eps_1 + eps_perm) - np.exp(eps_1 + 1.5 * eps_perm)))
+
+    if (np.array([p1, q1, p2, q2]) >= 0).all():
+        pass
+    else:
+        raise ValueError('Probabilities are negative.')
+
+    # Unary encoding
+    input_ue_data = np.zeros(k)
+    if input_data != None:
+        input_ue_data[input_data] = 1
+
+    # First round of sanitization (permanent memoization) with SUE using user's input_ue_data
+    first_sanitization = np.zeros(k)
+    for ind in range(k):
+        if input_ue_data[ind] != 1:
+            rnd = np.random.random()
+            if rnd <= q1:
+                first_sanitization[ind] = 1
+        else:
+            rnd = np.random.random()
+            if rnd <= p1:
+                first_sanitization[ind] = 1
+
+    # Second round of sanitization with OUE using first_sanitization as input
+    second_sanitization = np.zeros(k)
+    for ind in range(k):
+        if first_sanitization[ind] != 1:
+            rnd = np.random.random()
+            if rnd <= q2:
+                second_sanitization[ind] = 1
+        else:
+            rnd = np.random.random()
+            if rnd <= p2:
+                second_sanitization[ind] = 1
+
+    return second_sanitization
+
+def L_SOUE_Aggregator(ue_reports, eps_perm, eps_1):
+    """
+    Statistical Estimator for Normalized Frequency (0 -- 1) with post-processing to ensure non-negativity.
+
+    :param reports: list of all L-SOUE sanitized UE-based vectors;
+    :param eps_perm: upper bound of privacy guarantee (infinity reports);
+    :param eps_1: lower bound of privacy guarantee (a single report), thus, eps_1 < eps_perm;
+    :return: normalized frequency (histogram) estimation.
+    """
+
+    if len(ue_reports) == 0:
+        raise ValueError('List of reports is empty.')
+
+    # Number of reports
+    n = len(ue_reports)
+
+    # SUE parameters for round 1
+    p1 = np.exp(eps_perm / 2) / (np.exp(eps_perm / 2) + 1)
+    q1 = 1 - p1
+
+    # OUE parameters for round 2
+    p2 = 0.5
+    q2 = (3.35410196624968 * (np.exp(eps_1) - 1) * (
+            np.exp(0.5 * eps_perm) + 2 * np.exp(eps_perm) + np.exp(1.5 * eps_perm)) * np.sqrt(
+        0.0111111111111111 * np.exp(eps_1) + 0.00555555555555556 * np.exp(2 * eps_1) + 0.0444444444444444 * np.exp(
+            0.5 * eps_perm) + 0.155555555555556 * np.exp(eps_perm) + 0.311111111111111 * np.exp(
+            1.5 * eps_perm) + 0.388888888888889 * np.exp(2 * eps_perm) + 0.311111111111111 * np.exp(
+            2.5 * eps_perm) + 0.155555555555556 * np.exp(3 * eps_perm) + 0.0444444444444444 * np.exp(
+            3.5 * eps_perm) + 0.00555555555555556 * np.exp(4 * eps_perm) - 0.222222222222222 * np.exp(
+            eps_1 + eps_perm) - 0.711111111111111 * np.exp(eps_1 + 1.5 * eps_perm) - np.exp(
+            eps_1 + 2 * eps_perm) - 0.711111111111111 * np.exp(eps_1 + 2.5 * eps_perm) - 0.222222222222222 * np.exp(
+            eps_1 + 3 * eps_perm) + 0.0111111111111111 * np.exp(eps_1 + 4 * eps_perm) + 0.0444444444444444 * np.exp(
+            2 * eps_1 + 0.5 * eps_perm) + 0.155555555555556 * np.exp(2 * eps_1 + eps_perm) + 0.311111111111111 * np.exp(
+            2 * eps_1 + 1.5 * eps_perm) + 0.388888888888889 * np.exp(
+            2 * eps_1 + 2 * eps_perm) + 0.311111111111111 * np.exp(
+            2 * eps_1 + 2.5 * eps_perm) + 0.155555555555556 * np.exp(
+            2 * eps_1 + 3 * eps_perm) + 0.0444444444444444 * np.exp(
+            2 * eps_1 + 3.5 * eps_perm) + 0.00555555555555556 * np.exp(
+            2 * eps_1 + 4 * eps_perm) + 0.00555555555555556) - 0.25 * (
+                  np.exp(0.5 * eps_perm) + 2 * np.exp(eps_perm) + np.exp(1.5 * eps_perm) - np.exp(
+              eps_1 + 0.5 * eps_perm) - 2 * np.exp(eps_1 + eps_perm) - np.exp(eps_1 + 1.5 * eps_perm)) * (
+                  np.exp(eps_1) + 4 * np.exp(0.5 * eps_perm) + 4 * np.exp(eps_perm) - np.exp(
+              2 * eps_perm) - 4 * np.exp(eps_1 + eps_perm) - 4 * np.exp(eps_1 + 1.5 * eps_perm) - np.exp(
+              eps_1 + 2 * eps_perm) + 1)) / (
+                 (np.exp(eps_1) - 1) * (np.exp(0.5 * eps_perm) + 2 * np.exp(eps_perm) + np.exp(1.5 * eps_perm)) * (
+                 np.exp(0.5 * eps_perm) + 2 * np.exp(eps_perm) + np.exp(1.5 * eps_perm) - np.exp(
+             eps_1 + 0.5 * eps_perm) - 2 * np.exp(eps_1 + eps_perm) - np.exp(eps_1 + 1.5 * eps_perm)))
+
+    if (np.array([p1, q1, p2, q2]) >= 0).all():
+        pass
+    else:
+        raise ValueError('Probabilities are negative.')
+
+    # Ensure non-negativity of estimated frequency
+    est_freq = ((sum(ue_reports) - n * q1 * (p2 - q2) - n * q2) / (n * (p1 - q1) * (p2 - q2))).clip(0)
+
+    # Re-normalized estimated frequency
+    norm_est_freq = est_freq / sum(est_freq)
+
+    return norm_est_freq
