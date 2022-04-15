969fc40022c02fb71d574d0a35d62f4e84992bc7
Héber Hwang Arcolezi <55852688+hharcolezi@users.noreply.github.com>
2022-04-15 17:21:00 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+from setuptools import setup, find_packages
+import os
+
+with open("README.md", "r") as fh:
+    describe_package = fh.read()
+
+setup(
+    name='multi-freq-ldpy',
+    version='0.1.1',
+    license='MIT',
+    author="Héber H. Arcolezi",
+    author_email='hh.arcolezi@gmail.com',
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    url='https://github.com/hharcolezi/multi-freq-ldpy',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords='local-differential-privacy',
+    install_requires=[
+                        'numpy', 'numba'
+                     ],
+    description='Multiple Frequency Estimation Under Local Differential Privacy in Python',
+    long_description=describe_package,
+    python_requires=">=3.6",
+
+)
