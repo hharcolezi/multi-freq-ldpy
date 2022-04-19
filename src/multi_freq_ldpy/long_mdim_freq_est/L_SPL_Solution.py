4ae5038714b8410592a9d3d561e68c983c56f83a
hharcolezi <heber.hwang-arcolezi@inria.fr>
2022-04-19 14:06:25 +0200

    Update L_SPL_Solution.py

--- src/multi_freq_ldpy/long_mdim_freq_est/L_SPL_Solution.py
+++ src/multi_freq_ldpy/long_mdim_freq_est/L_SPL_Solution.py
@@ -2,7 +2,7 @@ import numpy as np
 from multi_freq_ldpy.long_freq_est.L_GRR import L_GRR_Client, L_GRR_Aggregator
 from multi_freq_ldpy.long_freq_est.L_OUE import L_OUE_Client, L_OUE_Aggregator
 from multi_freq_ldpy.long_freq_est.L_OSUE import L_OSUE_Client, L_OSUE_Aggregator
-from multi_freq_ldpy..L_SUE import L_SUE_Client, L_SUE_Aggregator
+from multi_freq_ldpy.long_freq_est.L_SUE import L_SUE_Client, L_SUE_Aggregator
 from multi_freq_ldpy.long_freq_est.L_SOUE import L_SOUE_Client, L_SOUE_Aggregator
 from multi_freq_ldpy.long_freq_est.L_ADP import L_ADP_Client, L_ADP_Aggregator
 
