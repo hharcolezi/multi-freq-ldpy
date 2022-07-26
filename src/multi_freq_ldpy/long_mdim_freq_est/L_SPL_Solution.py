c28a9c075d4af1af240972298d8445a2083cccb6
hharcolezi <hh.arcolezi@gmail.com>
2022-07-26 13:27:52 +0200

    Update L_SPL_Solution.py

--- src/multi_freq_ldpy/long_mdim_freq_est/L_SPL_Solution.py
+++ src/multi_freq_ldpy/long_mdim_freq_est/L_SPL_Solution.py
@@ -5,9 +5,11 @@ from multi_freq_ldpy.long_freq_est.L_OSUE import L_OSUE_Client, L_OSUE_Aggregato
 from multi_freq_ldpy.long_freq_est.L_SUE import L_SUE_Client, L_SUE_Aggregator
 from multi_freq_ldpy.long_freq_est.L_SOUE import L_SOUE_Client, L_SOUE_Aggregator
 from multi_freq_ldpy.long_freq_est.L_ADP import L_ADP_Client, L_ADP_Aggregator
+from multi_freq_ldpy.long_freq_est.dBitFlipPM import dBitFlipPM_Client, dBitFlipPM_Aggregator
 
 # [1] Arcolezi et al (2021) "Improving the Utility of Locally Differentially Private Protocols for Longitudinal and Multidimensional Frequency Estimates" (arXiv:2111.04636).
 # [2] Erlingsson, Pihur, and Korolova (2014) "RAPPOR: Randomized aggregatable privacy-preserving ordinal response" (ACM CCS).
+# [3] Ding, Kulkarni, and Yekhanin (2017) "Collecting telemetry data privately." (NeurIPS).
 
 def SPL_L_GRR_Client(input_tuple, lst_k, d, eps_perm, eps_1):
 
@@ -126,6 +128,30 @@ def SPL_L_SOUE_Client(input_tuple, lst_k, d, eps_perm, eps_1):
         sanitized_tuple.append(L_SOUE_Client(input_tuple[idx], lst_k[idx], eps_perm_spl, eps_1_spl))
 
     return sanitized_tuple
+    
+def SPL_dBitFlipPM_Client(input_tuple, lst_k, lst_b, d_bits, d, eps_perm):
+
+    """
+    Splitting (SPL) the privacy budget and using dBitFlipPM [3] protocol as local randomizer.
+
+    :param input_tuple: user's true tuple of values;
+    :param lst_k: list of attributes' domain size;
+    :param lst_b: list of attributes' new domain size (bucketized);
+    :param d_bits: number of bits to report;
+    :param d: number of attributes;
+    :param eps_perm: upper bound of privacy guarantee (infinity reports);
+    :return: tuple of sanitized UE vectors.
+    """
+
+    # Splitting the privacy budget over the number of attributes d
+    eps_perm_spl = eps_perm / d
+
+    # Sanitization of each value with L-GRR protocol
+    sanitized_tuple = []
+    for idx in range(d):
+        sanitized_tuple.append(dBitFlipPM_Client(input_tuple[idx], lst_k[idx], lst_b[idx], d_bits, eps_perm_spl))
+
+    return sanitized_tuple    
 
 def SPL_L_ADP_Client(input_tuple, lst_k, d, eps_perm, eps_1):
 
@@ -227,6 +253,32 @@ def SPL_L_OSUE_Aggregator(reports_tuple, d, eps_perm, eps_1):
     for idx in range(d):
         reports = reports_tuple[:, idx]
         lst_freq_est.append(L_OSUE_Aggregator(reports, eps_perm_spl, eps_1_spl))
+
+    return np.array(lst_freq_est, dtype='object')
+    
+def SPL_dBitFlipPM_Aggregator(reports_tuple, lst_b, d_bits, d, eps_perm):
+
+    """
+    Statistical Estimator for Normalized Frequency (0 -- 1) of all d attributes with post-processing to ensure non-negativity.
+
+    :param reports_tuple: list of all sanitized tuples;
+    :param lst_b: list of attributes' new domain size (bucketized);
+    :param d_bits: number of bits to report;
+    :param d: number of attributes;
+    :param eps_perm: upper bound of privacy guarantee (infinity reports);
+    :return: normalized frequency (histogram) estimation of all d attributes.
+    """
+
+    reports_tuple = np.array(reports_tuple, dtype='object')
+
+    # Splitting the privacy budget over the number of attributes d
+    eps_perm_spl = eps_perm / d
+
+    # Estimated frequency for all d attributes
+    lst_freq_est = []
+    for idx in range(d):
+        reports = reports_tuple[:, idx]
+        lst_freq_est.append(dBitFlipPM_Aggregator(reports, lst_b[idx], d_bits, eps_perm_spl))
 
     return np.array(lst_freq_est, dtype='object')
 
