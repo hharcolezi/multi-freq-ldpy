806cb561cc76ece374e47dbb65b2bfa51898a6d6
hharcolezi <hh.arcolezi@gmail.com>
2022-07-26 13:31:51 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='multi-freq-ldpy',
-    version='0.2.3',
+    version='0.2.4',
     license='MIT',
     author="Héber H. Arcolezi",
     author_email='hh.arcolezi@gmail.com',
