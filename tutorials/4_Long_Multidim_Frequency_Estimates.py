e70b061e755d041ccc0cd5e3de1d442077b55e31
hharcolezi <hh.arcolezi@gmail.com>
2022-07-26 14:55:05 +0200

    update tutorial4

--- tutorials/4_Long_Multidim_Frequency_Estimates.py
+++ tutorials/4_Long_Multidim_Frequency_Estimates.py
@@ -39,7 +39,7 @@ from multi_freq_ldpy.long_mdim_freq_est.L_SMP_Solution import *
 
 # ## Usage Example
 
-# In[12]:
+# In[3]:
 
 
 d = 3 # number of attributes
@@ -48,16 +48,21 @@ input_data = [0, 3, 5] # real input values
 eps_perm = 2 # epsilon infinity (infinity reports -- upper bound)
 eps_1 = 0.05 # epsilon 1 (single report -- lower bound)
 
+d_bits = 1 # nb of bits for dBitFlipPM protocol
+lst_b = lst_k # new (bucketized) number of values per attribute
+
 print('Real value:', input_data)
 print('Sanitization w/ SPL solution and L-GRR protocol:', SPL_L_GRR_Client(input_data, lst_k, d, eps_perm, eps_1)) 
 print('Sanitization w/ SPL solution and L-SUE protocol:', SPL_L_SUE_Client(input_data, lst_k, d, eps_perm, eps_1))
 print('Sanitization w/ SMP solution and L-GRR protocol:', SMP_L_GRR_Client(input_data, lst_k, d, eps_perm, eps_1))
 print('Sanitization w/ SMP solution and L-SUE protocol:', SMP_L_SUE_Client(input_data, lst_k, d, eps_perm, eps_1))
+print('Sanitization w/ SPL solution and dBitFlipPM protocol:', (SPL_dBitFlipPM_Client(input_data, lst_k, lst_b, d_bits, d, eps_perm)))
+print('Sanitization w/ SMP solution and dBitFlipPM protocol:', SMP_dBitFlipPM_Client(input_data, lst_k, lst_b, d_bits, d, eps_perm))
 
 
 # ## Reading Nursery dataset
 
-# In[3]:
+# In[4]:
 
 
 df = pd.read_csv('datasets/db_nursery.csv')
@@ -66,7 +71,7 @@ df
 
 # ## Encoding values
 
-# In[4]:
+# In[5]:
 
 
 LE = LabelEncoder()
@@ -81,7 +86,7 @@ df
 
 # ## Static Parameteres
 
-# In[5]:
+# In[6]:
 
 
 # number of users
@@ -96,6 +101,13 @@ print('Number of Attributes =', d)
 lst_k = [len(df[att].unique()) for att in attributes]
 print('Domain size of attributes =', lst_k)
 
+print('\ndBitFlipPM parameters')
+lst_b = lst_k # Number of buckets <= k
+print("New (bucketized) domain size of attributes =", lst_b)
+
+d_bits = 1 # Number of bits each user sample <=b
+print("Number of bits d_bits:", d_bits)
+
 print("\nPrivacy guarantees:")
 
 # upper bound (infinity reports)
@@ -109,7 +121,7 @@ print("List of epsilon_1 =", lst_eps_1)
 
 # ## Comparison of multidimensional solutions with longitudinal protocols
 
-# In[6]:
+# In[7]:
 
 
 # Real normalized frequencies
@@ -120,11 +132,11 @@ nb_seed = 30
 
 # Save Averaged Mean Squared Error (MSE_avg) between real and estimated frequencies per seed
 dic_avg_mse = {seed: 
-                   {"SPL_L_GRR": [], "SPL_L_OUE": [], "SPL_L_OSUE": [], "SPL_L_SUE": [], "SPL_L_SOUE": [], "SPL_L_ADP": [], 
-                    "SMP_L_GRR": [], "SMP_L_OUE": [], "SMP_L_OSUE": [], "SMP_L_SUE": [], "SMP_L_SOUE": [], "SMP_L_ADP": []
-                   } 
-                   for seed in range(nb_seed)
-              }
+{"SPL_L_GRR": [], "SPL_L_OUE": [], "SPL_L_OSUE": [], "SPL_L_SUE": [], "SPL_L_SOUE": [], "SPL_dBitFlipPM": [], "SPL_L_ADP": [], 
+"SMP_L_GRR": [], "SMP_L_OUE": [], "SMP_L_OSUE": [], "SMP_L_SUE": [], "SMP_L_SOUE": [], "SMP_dBitFlipPM": [], "SMP_L_ADP": []
+} 
+for seed in range(nb_seed)
+      }
 
 starttime = time.time()
 for seed in range(nb_seed):
@@ -148,9 +160,9 @@ for seed in range(nb_seed):
 #         spl_est_freq = SPL_L_OSUE_Aggregator(spl_reports, d, eps_perm, eps_1)
 #         dic_avg_mse[seed]["SPL_L_OSUE"].append(np.mean([mean_squared_error(real_freq[att], spl_est_freq[att]) for att in range(d)]))
         
-        spl_reports = [SPL_L_SUE_Client(input_data, lst_k, d, eps_perm, eps_1) for input_data in df.values]
-        spl_est_freq = SPL_L_SUE_Aggregator(spl_reports, d, eps_perm, eps_1)
-        dic_avg_mse[seed]["SPL_L_SUE"].append(np.mean([mean_squared_error(real_freq[att], spl_est_freq[att]) for att in range(d)]))
+#         spl_reports = [SPL_L_SUE_Client(input_data, lst_k, d, eps_perm, eps_1) for input_data in df.values]
+#         spl_est_freq = SPL_L_SUE_Aggregator(spl_reports, d, eps_perm, eps_1)
+#         dic_avg_mse[seed]["SPL_L_SUE"].append(np.mean([mean_squared_error(real_freq[att], spl_est_freq[att]) for att in range(d)]))
         
 #         spl_reports = [SPL_L_SOUE_Client(input_data, lst_k, d, eps_perm, eps_1) for input_data in df.values]
 #         spl_est_freq = SPL_L_SOUE_Aggregator(spl_reports, d, eps_perm, eps_1)
@@ -159,6 +171,10 @@ for seed in range(nb_seed):
         spl_reports = [SPL_L_ADP_Client(input_data, lst_k, d, eps_perm, eps_1) for input_data in df.values]
         spl_est_freq = SPL_L_ADP_Aggregator(spl_reports, lst_k, d, eps_perm, eps_1)
         dic_avg_mse[seed]["SPL_L_ADP"].append(np.mean([mean_squared_error(real_freq[att], spl_est_freq[att]) for att in range(d)]))
+        
+        spl_reports = [SPL_dBitFlipPM_Client(input_data, lst_k, lst_b, d_bits, d, eps_perm) for input_data in df.values]
+        spl_est_freq = SPL_dBitFlipPM_Aggregator(spl_reports, lst_b, d_bits, d, eps_perm)
+        dic_avg_mse[seed]["SPL_dBitFlipPM"].append(np.mean([mean_squared_error(real_freq[att], spl_est_freq[att]) for att in range(d)]))
 
         # SMP solution 
 #         smp_reports = [SMP_L_GRR_Client(input_data, lst_k, d, eps_perm, eps_1) for input_data in df.values]
@@ -173,9 +189,9 @@ for seed in range(nb_seed):
 #         smp_est_freq = SMP_L_OSUE_Aggregator(smp_reports, d, eps_perm, eps_1)
 #         dic_avg_mse[seed]["SMP_L_OSUE"].append(np.mean([mean_squared_error(real_freq[att], smp_est_freq[att]) for att in range(d)]))
         
-        smp_reports = [SMP_L_SUE_Client(input_data, lst_k, d, eps_perm, eps_1) for input_data in df.values]
-        smp_est_freq = SMP_L_SUE_Aggregator(smp_reports, d, eps_perm, eps_1)
-        dic_avg_mse[seed]["SMP_L_SUE"].append(np.mean([mean_squared_error(real_freq[att], smp_est_freq[att]) for att in range(d)]))
+#         smp_reports = [SMP_L_SUE_Client(input_data, lst_k, d, eps_perm, eps_1) for input_data in df.values]
+#         smp_est_freq = SMP_L_SUE_Aggregator(smp_reports, d, eps_perm, eps_1)
+#         dic_avg_mse[seed]["SMP_L_SUE"].append(np.mean([mean_squared_error(real_freq[att], smp_est_freq[att]) for att in range(d)]))
         
 #         smp_reports = [SMP_L_SOUE_Client(input_data, lst_k, d, eps_perm, eps_1) for input_data in df.values]
 #         smp_est_freq = SMP_L_SOUE_Aggregator(smp_reports, d, eps_perm, eps_1)
@@ -185,12 +201,16 @@ for seed in range(nb_seed):
         smp_est_freq = SMP_L_ADP_Aggregator(smp_reports, lst_k, d, eps_perm, eps_1)
         dic_avg_mse[seed]["SMP_L_ADP"].append(np.mean([mean_squared_error(real_freq[att], smp_est_freq[att]) for att in range(d)]))       
         
+        smp_reports = [SMP_dBitFlipPM_Client(input_data, lst_k, lst_b, d_bits, d, eps_perm) for input_data in df.values]
+        smp_est_freq = SMP_dBitFlipPM_Aggregator(smp_reports, lst_b, d_bits, d, eps_perm)
+        dic_avg_mse[seed]["SMP_dBitFlipPM"].append(np.mean([mean_squared_error(real_freq[att], smp_est_freq[att]) for att in range(d)]))       
+        
 print('That took {} seconds'.format(time.time() - starttime))        
 
 
 # ## Plotting metrics results
 
-# In[7]:
+# In[8]:
 
 
 plt.figure(figsize=(8,5))
@@ -201,16 +221,18 @@ plt.plot(np.mean([dic_avg_mse[seed]["SPL_L_ADP"] for seed in range(nb_seed)], ax
 # plt.plot(np.mean([dic_avg_mse[seed]["SPL_L_GRR"] for seed in range(nb_seed)], axis=0), label='SPL_L_GRR', marker='>',linestyle='dashed')
 # plt.plot(np.mean([dic_avg_mse[seed]["SPL_L_OUE"] for seed in range(nb_seed)], axis=0), label='SPL_L_OUE', marker='s',linestyle='dotted')
 # plt.plot(np.mean([dic_avg_mse[seed]["SPL_L_OSUE"] for seed in range(nb_seed)], axis=0), label='SPL_L_OSUE', marker='D', linestyle=(0, (3, 10, 1, 10)))
-plt.plot(np.mean([dic_avg_mse[seed]["SPL_L_SUE"] for seed in range(nb_seed)], axis=0), label='SPL_L_SUE', marker='d',linestyle=(0, (5, 10)))
+# plt.plot(np.mean([dic_avg_mse[seed]["SPL_L_SUE"] for seed in range(nb_seed)], axis=0), label='SPL_L_SUE', marker='d',linestyle=(0, (5, 10)))
 # plt.plot(np.mean([dic_avg_mse[seed]["SPL_L_SOUE"] for seed in range(nb_seed)], axis=0), label='SPL_L_SUE', marker='^',linestyle=(0, (3, 1, 1, 1, 1, 1)))
+plt.plot(np.mean([dic_avg_mse[seed]["SPL_dBitFlipPM"] for seed in range(nb_seed)], axis=0), label='SPL_dBitFlipPM', marker='^',linestyle='dashed')
 
 # SMP solution
 plt.plot(np.mean([dic_avg_mse[seed]["SMP_L_ADP"] for seed in range(nb_seed)], axis=0), label='SMP_L_ADP', marker='s',linestyle='dotted')
 # plt.plot(np.mean([dic_avg_mse[seed]["SMP_L_GRR"] for seed in range(nb_seed)], axis=0), label='SMP_L_GRR', marker='d',linestyle=(0, (5, 10)))
 # plt.plot(np.mean([dic_avg_mse[seed]["SMP_L_OUE"] for seed in range(nb_seed)], axis=0), label='SMP_L_OUE',marker='s',linestyle='dotted')
 # plt.plot(np.mean([dic_avg_mse[seed]["SMP_L_OSUE"] for seed in range(nb_seed)], axis=0), label='SMP_L_OSUE', marker='D', linestyle=(0, (3, 10, 1, 10)))
-plt.plot(np.mean([dic_avg_mse[seed]["SMP_L_SUE"] for seed in range(nb_seed)], axis=0), label='SMP_L_SUE',marker='d',linestyle=(0, (5, 10)))
+# plt.plot(np.mean([dic_avg_mse[seed]["SMP_L_SUE"] for seed in range(nb_seed)], axis=0), label='SMP_L_SUE',marker='d',linestyle=(0, (5, 10)))
 # plt.plot(np.mean([dic_avg_mse[seed]["SMP_L_SOUE"] for seed in range(nb_seed)], axis=0), label='SMP_L_SOUE', marker='^',linestyle=(0, (3, 1, 1, 1, 1, 1)))
+plt.plot(np.mean([dic_avg_mse[seed]["SMP_dBitFlipPM"] for seed in range(nb_seed)], axis=0), label='SMP_dBitFlipPM', marker='^',linestyle='dashed')
 
 plt.yscale('log')
 plt.xlabel('$\epsilon_{perm}$')
@@ -222,7 +244,7 @@ plt.show();
 
 # ## Example of Real vs Estimated Freqencies
 
-# In[8]:
+# In[9]:
 
 
 plt.figure(figsize=(12, 5))
